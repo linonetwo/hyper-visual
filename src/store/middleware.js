4f96adc2535eb332146d6ab13b0265c47a0e1a1c
lindongwu <lindongwu@memect.co>
2018-05-13 20:50:20 +0800

    feat: list items on start up

--- src/store/middleware.js
+++ src/store/middleware.js
@@ -1,9 +1,9 @@
-import { SESSION_USER_DATA, searchAffordances } from './actions';
+import { SESSION_ADD, SESSION_USER_DATA, searchAffordances } from './actions';
 
 export default store => next => action => {
   // search affordances whenever user input changes
   // TODO: listening on cwd change
-  if (action.type === SESSION_USER_DATA) {
+  if (action.type === SESSION_USER_DATA || action.type === SESSION_ADD) {
     store.dispatch(searchAffordances());
   }
   next(action);
