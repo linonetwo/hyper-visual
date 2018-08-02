6857835dd17f84b4cabd179a9dbdb0d8b56e51bd
linonetwo <linonetwo012@gmail.com>
2018-08-02 17:52:02 +0800

    bugfix: remove keybinding
    https://github.com/soutar/hyperterm-register-shortcut/issues/1

--- src/onApp.js
+++ src/onApp.js
@@ -1,13 +1,8 @@
 // import registerShortcut from 'hyperterm-register-shortcut';
 
-import { PLUGIN, TOGGLE_GUI } from './store/actions';
+import { TOGGLE_GUI } from './store/actions';
 
-// look for config[PLUGIN].hotkey
-const configKey = PLUGIN;
-const defaultShortcut = 'Ctrl+G';
-
-function toggleGUI(app) {
+export function toggleGUI(app) {
   [...app.getWindows()][0].rpc.emit(TOGGLE_GUI);
 }
 
-// export default registerShortcut(configKey, toggleGUI, defaultShortcut);
